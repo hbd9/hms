c8bb98c1b5d87ea96abd35709c36dc7dc60ebf9d
heamanbelay <heamanbelay@gmail.com>
2022-04-03 22:53:57 +0300

    portfolio commit

--- server.js
+++ server.js
@@ -1,14 +1,21 @@
-const http = require('http');
+const express = require('express');
+const bodyParsor = require('body-parser');
+const hbs = require('hbs');
+const path = require('path');
+const app = express();
+//view engine support
+app.use(express.static(path.join(__dirname,'public')));
+app.set('views',path.join(__dirname,'views'));
+hbs.registerPartials(__dirname + "/views/partials");
+app.set('view engine','hbs');
 
-const hostname = '127.0.0.1';
-const port = 3000;
+app.use(bodyParsor.urlencoded({extended:false}));
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
-});
+app.use(bodyParsor.json());
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+const indexRoute = require('./routes/index_route');
+
+app.use(indexRoute);
+app.listen(3000,()=>{
+  console.log("Server running at port 3000")
+})
